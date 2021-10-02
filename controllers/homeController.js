1203a3aa562cdba819e75f0cc17c456baa8d43a3
eferreccio <estebanferreccio@gmail.con>
2021-10-02 09:58:14 -0300

    separando estilos header y footer - agregado producto.json (vista dinam.)

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,11 @@
+const fs = require('fs');
+
+let productosJson = fs.readFileSync("./public/js/productos.json", 'utf-8') ;
+let productos = JSON.parse(productosJson);
+
 const controlHome = {
     mostrar: function(req,res){
-        res.render('home');
+        res.render('home',{productos});
     },
     listarProductos: function(){},
     novedades: function(){},
